7b4818e42261d6dd5ddaa819511657936494b6b0
quguocheng <quguocheng@meituan.com>
2016-06-14 13:22:16 +0800

    添加token，对于叶子节点来说，只有一个token。对于非叶子节点来说，不必保存token

--- src/main/java/hivesql/analysis/node/MyAstNode.java
+++ src/main/java/hivesql/analysis/node/MyAstNode.java
@@ -1,9 +1,12 @@
 package hivesql.analysis.node;
 
+import org.antlr.v4.runtime.Token;
+
 import common.ast.AstNode;
 
 public class MyAstNode extends AstNode {
 	private AstNodeType nodeType;
+	private transient Token token;
 	
 	public MyAstNode(){
 	}
@@ -20,6 +23,14 @@ public class MyAstNode extends AstNode {
 		this.nodeType = nodeType;
 	}
 	
+	public Token getToken() {
+		return token;
+	}
+
+	public void setToken(Token token) {
+		this.token = token;
+	}
+
 	@Override
 	public String toString(){
 		return String.format("{node_type:%s, name:%s, source_interval:%s}", getNodeType(), getName(), getSourceInterval());
