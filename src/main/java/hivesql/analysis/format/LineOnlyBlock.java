6276c4262da70e4ca309aeb44c1e18fe10fc260b
quguocheng <quguocheng@meituan.com>
2016-06-16 16:11:14 +0800

    LineOnlyBlock: contains only newlines

---
+++ src/main/java/hivesql/analysis/format/LineOnlyBlock.java
@@ -0,0 +1,35 @@
+package hivesql.analysis.format;
+
+public final class LineOnlyBlock extends Block {
+	
+	private int lineCount;
+
+	public int getLineCount() {
+		return lineCount;
+	}
+	public void setLineCount(int lineCount) {
+		this.lineCount = lineCount;
+	}
+	public static LineOnlyBlock buildOne(int sc){
+		LineOnlyBlock b = new LineOnlyBlock();
+		b.setSpaceCount(sc);
+		b.setLineCount(1);
+		return b;
+	}
+	public static LineOnlyBlock build(int sc, int lc){
+		LineOnlyBlock b = new LineOnlyBlock();
+		b.setLineCount(lc);
+		return b;
+	}
+	
+	@Override
+	public String show() {
+		StringBuilder ret = new StringBuilder();
+		String spaces = buildSpaces(getSpaceCount());
+		for(int i=0;i<getLineCount();i++){
+			ret.append(spaces+"\n");
+		}
+		return ret.toString();
+	}
+
+}
